1d64e9c37eba14a449fab29aab7e865dcd12d4b9
YIGIT-LPT-8123003 <user@YIGIT-LPT-8123003.yig.local>
2023-07-03 11:40:14 +0630

    first

--- App.js
+++ App.js
@@ -1,20 +1,182 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import * as SQLite from "expo-sqlite";
+import * as Sharing from "expo-sharing";
+import * as FileSystem from "expo-file-system";
+import * as DocumentPicker from "expo-document-picker";
+const App = () => {
+  var [db, setDb] = useState(SQLite.openDatabase("manage.db"));
+  const [isloading, setIsloading] = useState(true);
+  const [total, setTotal] = useState([]);
+  const [currentTotal, setCurrentTotal] = useState(undefined);
+  const exportDb = async () => {
+    await Sharing.shareAsync(FileSystem.documentDirectory + "SQLite/manage.db");
+  };
+  const importDb = async () => {
+    let result = await DocumentPicker.getDocumentAsync({
+      copyToCacheDirectory: true,
+    });
+    if (result.type === "success") {
+      setIsloading(true);
+      if (
+        !(
+          await FileSystem.getInfoAsync(FileSystem.documentDirectory + "SQLite")
+        ).exists
+      ) {
+        await FileSystem.makeDirectoryAsync(
+          FileSystem.documentDirectory + "SQLite"
+        );
 
-export default function App() {
+        const base64 = await FileSystem.readAsStringAsync(result.uri, {
+          encoding: FileSystem.EncodingType.Base64,
+        });
+        await FileSystem.writeAsStringAsync(
+          FileSystem.documentDirectory + "SQLite/manage.db",
+          base64,
+          { encoding: FileSystem.EncodingType.Base64 }
+        );
+        db.closeAsync();
+        setDb(SQLite.openDatabase("manage.db"));
+      }
+    }
+  };
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS total(id INTEGER PRIMARY KEY AUTOINCREMENT,total TEXT,date TEXT)"
+      );
+      tx.executeSql(
+        "SELECT * FROM total",
+        null,
+        (txObj, resultSet) => setTotal(resultSet.rows._array),
+        (txObj, error) => console.log(error)
+      );
+    });
+    setIsloading(false);
+  }, []);
+
+  if (isloading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading Total....</Text>
+      </View>
+    );
+  }
+  const showTotal = () => {
+    return total.map((tt, index) => {
+      return (
+        <View key={index} style={styles.forrow}>
+          <Text>{tt.total}</Text>
+          <Text>{tt.date}</Text>
+          <Button title="Delete" onPress={() => deteteTotal(tt.id)} />
+          <Button title="Update" onPress={() => updateTotal(tt.id)} />
+        </View>
+      );
+    });
+  };
+  const addTotal = () => {
+    const currentDate = new Date();
+    const today = currentDate.toDateString();
+    db.transaction((tx) => {
+      tx.executeSql(
+        "INSERT INTO total (total,date) VALUES (?,?)",
+        [currentTotal, today],
+        (txobj, resultSet) => {
+          let existingTotals = [...total];
+          existingTotals.push({
+            id: resultSet.insertId,
+            total: currentTotal,
+            date: today,
+          });
+          setTotal(existingTotals);
+          setCurrentTotal(undefined);
+        },
+        (txObj, error) => console.log(error)
+      );
+    });
+  };
+
+  const deteteTotal = (id) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "DELETE FROM total WHERE id=?",
+        [id],
+        (txObj, resultSet) => {
+          console.log("Deleted");
+
+          if (resultSet.rowsAffected > 0) {
+            let existingTotals = [...total].filter((tot) => tot.id !== id);
+            setTotal(existingTotals);
+          }
+        },
+        (txObj, error) => {
+          console.log(error);
+        }
+      );
+    });
+  };
+  const updateTotal = (id) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "UPDATE total SET total = ? WHERE id =?",
+        [currentTotal, id],
+        (txObj, resultSet) => {
+          if (resultSet.rowsAffected > 0) {
+            let existingTotals = [...total];
+            const uptodateTotal = existingTotals.findIndex(
+              (total) => total.id == id
+            );
+            existingTotals[uptodateTotal].total = currentTotal;
+            setTotal(existingTotals);
+            setCurrentTotal(undefined);
+          }
+        },
+        (txObj, error) => {
+          console.log(error);
+        }
+      );
+    });
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput
+        value={currentTotal}
+        placeholder="Enter Total Amount"
+        onChangeText={setCurrentTotal}
+      />
+      <Button title="Add Total" onPress={addTotal} />
+      {showTotal()}
+      <Button title="Export Data" onPress={exportDb} />
+      <Button title="Import Data" onPress={importDb} />
+
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  forrow: {
+    flexDirection: "row",
+    alignItems: "center",
+    alignSelf: "stretch",
+    justifyContent: "space-between",
+    margin: 8,
+  },
+  button: {
+    color: "red",
   },
 });
